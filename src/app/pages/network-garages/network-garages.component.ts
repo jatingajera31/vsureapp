38ed48e31387b87b04f30ad457fbec3ea791403e
jatin gajera <jgajera@markettime.com>
2023-12-21 16:43:14 +0530

    network garages api integrate

--- src/app/pages/network-garages/network-garages.component.ts
+++ src/app/pages/network-garages/network-garages.component.ts
@@ -10,25 +10,45 @@ export class NetworkGaragesComponent implements OnInit {
 
   constructor(private ApiServicesService:ApiServicesService) { }
   loginData:any;
-  
+  latitude:any;
+  longitude:any;
+  garage:any;
+
   ngOnInit(): void {
     this.loginData = JSON.parse(localStorage.getItem('loginToken'))
     this.getNearBygarage()
   }
 
   getNearBygarage(){
-    let asset = {
-      "ServiceType": "PreInspection",
-      "VehicleType": "PrivateCar",
-      "Pincode":"400086",
-      "State":"Bihar",
-      "City":"Bhojpur"
-  }
-    this.ApiServicesService.getToken().subscribe((data:any)=>{
-      this.ApiServicesService.getNetworkGarage(asset, data).subscribe((data:any)=>{
-        // this.vehicalList = JSON.parse(data)[0].Details[0].CustomerAssets
-         console.log("near garage",data);
-      })
-    })
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          console.log('position',this.latitude,this.longitude);
+          let asset = {
+            "ServiceType": "PreInspection",
+            "VehicleType": "",
+            "Latitude":this.latitude,
+            "Longitude":this.longitude,
+            "Regionid":"",
+            "Pincode":"",
+            "State":"",
+            "City":""
+          }
+          this.ApiServicesService.getToken().subscribe((data:any)=>{
+            this.ApiServicesService.getNetworkGarage(asset, data).subscribe((data:any)=>{
+              this.garage = JSON.parse(data)
+               console.log("near garage",JSON.parse(data));
+            })
+          })
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
   }
 }
